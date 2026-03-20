8644eef7778ab0104d923e59699a70dd84668922
iamsanka <iamsankadesilva@gmail.com>
2026-03-20 10:23:18 +0200

    Google Analytics

--- taprobaneEntertainment/src/App.tsx
+++ taprobaneEntertainment/src/App.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./components/Navbar/Navbar";
 import Hero from "./components/Hero/Hero";
 import Events from "./components/Events/events";
@@ -6,11 +6,29 @@ import AboutUs from "./components/AboutUs/AboutUs";
 import Contact from "./components/Contact/Contact";
 
 const App: React.FC = () => {
+  useEffect(() => {
+    // Load the Google Analytics script
+    const script = document.createElement("script");
+    script.async = true;
+    script.src = "https://www.googletagmanager.com/gtag/js?id=G-3FKCC569B4";
+    document.head.appendChild(script);
+
+    // Initialize GA
+    const inlineScript = document.createElement("script");
+    inlineScript.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-3FKCC569B4');
+    `;
+    document.head.appendChild(inlineScript);
+  }, []);
+
   return (
     <>
       <Navbar />
       <Hero />
-      {/**<Events />**/}
+      {/* <Events /> */}
       <AboutUs />
       <Contact />
     </>
